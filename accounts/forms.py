c9291e860e81cd2053e96fb0ea1141e6a418fb03
Almas-Forhadi <almasforhasi1999@gmail.com>
2025-11-07 22:11:07 +0600

    set unique username & mail

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,24 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 class SignupForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+    # Validate username uniqueness
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise ValidationError("This username is already taken.")
+        return username
+
+
+    # Validate email uniqueness
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("This email is already registered.")
+        return email
