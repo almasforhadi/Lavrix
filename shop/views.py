3cb1cf41dab6f65328a6534aec4561c0afd8ae66
Almas-Forhadi <almasforhasi1999@gmail.com>
2025-11-08 13:21:11 +0600

    media file added

--- shop/views.py
+++ shop/views.py
@@ -3,7 +3,6 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from .models import Category, Product, ProductImage
-from .forms import ProductForm, CategoryForm
 from django.core.paginator import Paginator
 from django.http import JsonResponse
 
